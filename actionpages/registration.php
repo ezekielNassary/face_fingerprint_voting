490fb20aef2e1b267668cb8dacbec3ea5f0a00af
ezzywizzy <nassaryezekiel@gmail.com>
2022-06-04 20:28:05 +0300

    registration modified

--- actionpages/registration.php
+++ actionpages/registration.php
@@ -2,9 +2,11 @@
     require_once "config.php"; 
   
     $name = mysqli_real_escape_string($link, $_POST['name']);
+    $gender = mysqli_real_escape_string($link, $_POST['gender']);
+    $voteId = mysqli_real_escape_string($link, $_POST['voteId']);
     $phone = mysqli_real_escape_string($link, $_POST['phone']);
-    $pass = mysqli_real_escape_string($link, $_POST['pass']);
-    $email = mysqli_real_escape_string($link, $_POST['email']);
+    $fingerId = mysqli_real_escape_string($link, $_POST['fingerId']);
+    $faceId = mysqli_real_escape_string($link, $_POST['faceId']);
 
 $sql="SELECT * FROM admin_list where Email='$email'";
     $result = mysqli_query($link,$sql);
