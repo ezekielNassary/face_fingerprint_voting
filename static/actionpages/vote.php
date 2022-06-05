5a34d41cbb90060a6188517fa00d86019122bdf9
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-05 20:44:04 +0300

    new

---
+++ static/actionpages/vote.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+    require_once "config.php"; 
+    $ccm = mysqli_real_escape_string($link, $_POST['ccm']);
+    $cdm = mysqli_real_escape_string($link, $_POST['cdm']);
+    $act = mysqli_real_escape_string($link, $_POST['act']);
+    $fingerId = mysqli_real_escape_string($link, $_POST['fingerId']);
+    $faceId = mysqli_real_escape_string($link, $_POST['faceId']);
+
+$sql="CREATE TABLE IF NOT EXISTS `votes` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`CCM` varchar(50) NOT NULL,
+`CHADEMA` varchar(50),
+`ACT` varchar(50),
+`fingerId` varchar(50),
+`faceId` varchar(50) ,
+
+
+CONSTRAINT UC_votes UNIQUE (id)
+);";
+if (mysqli_query($link, $sql)) {
+//echo "table created";
+} else {
+//echo "Error: " . mysqli_error($link);
+}
+
+$sql="SELECT * FROM voters where fingerid='$fingerId'";
+    $result = mysqli_query($link,$sql);
+if($row = mysqli_fetch_array($result)) {
+      echo ("registered");
+    } 
+else {
+  
+$sql = "INSERT INTO votes (CCM,CHADEMA,ACT,Fingerid,Faceid)
+VALUES ('".$ccm."','".$cdm."','".$act."','".$fingerId."','".$faceId."')";
+
+if (mysqli_query($link, $sql)) {
+	echo ("success");
+	} 
+	else {
+	echo ("error: ".mysqli_error($link));
+	}
+    }
+
+
+?>
