8ff3bd66841a80949a7ce727fe62332421cf2fb6
ezzywizzy <nassaryezekiel@gmail.com>
2022-06-05 18:14:43 +0300

    new version

--- app.py
+++ app.py
@@ -13,7 +13,15 @@ from typing import List
 import asyncio
 from threading import Thread
 import subprocess
+import time
+import board
+import random
+#import busio
+from digitalio import DigitalInOut, Direction
+import adafruit_fingerprint
 
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
 import websockets
 
 
@@ -32,25 +40,15 @@ app.mount("/static", StaticFiles(directory=st_abs_file_path, html=True), name="s
 
 
 clients = set()
+import serial
+uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
 
+finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
 
 @app.route("/")
 async  def index(request):
     return FileResponse(st_abs_file_path + "index.html")
 
-ports = get_serial_ports()
-
-_port = None
-serial_port = None
-for port, desc, hwid in sorted(ports):
-    logger.info("{}: {} [{}]".format(port, desc, hwid))
-    if "USB-Serial" in desc:
-        _port = port
-        break
-
-
-
-serial_port = serial.Serial(_port, baudrate=9600, timeout=0)
 
 
 @app.websocket_route("/ws")
@@ -61,62 +59,185 @@ async def websocket_endpoint(websocket: WebSocket):
         while True:
             is_running = True
             # await websocket.send_json({"message": "ping"})
-            # data = serial_port.readline()
-            # data = data.decode()
-            # if data != '':
-            #     logger.info("Received Data from Serial Port: {}".format(data))
-            #     serial_port.flushInput()
+            print("----------------")
+            if finger.read_templates() != adafruit_fingerprint.OK:
+                raise RuntimeError("Failed to read templates")
+            print("Fingerprint templates:", finger.templates)
+            print("e) enroll print")
+            print("f) find print")
+            print("d) delete print")
+            print("----------------")
+            c = await websocket.receive_text()
 
-            async def read_serial():
-                while is_running:
-
-                    try:
-                        if serial_port.inWaiting() > 0:
-                            data = serial_port.readline()
-                            data = data.decode()
-                            await websocket.send_text(data)
-                            logger.info("Received Data from Serial Port: {}".format(data))
-                            serial_port.flushInput()
-                    except Exception as e:
-                        try:
-                            if serial_port is not None and not serial_port.is_open:
-                                serial_port.open()
-                        except Exception as e:
-                            pass
-                        logger.error(f"Error in sending data {e}", exc_info=True)
+            if c == "e":
+                await enroll_finger(get_num(),websocket)
+            if c == "f":
+                if get_fingerprint():
+                    print("Detected #", finger.finger_id, "with confidence", finger.confidence)
+                else:
+                    print("Finger not found")
+            if c == "d":
+                if finger.delete_model(get_num()) == adafruit_fingerprint.OK:
+                    print("Deleted!")
+                else:
+                    print("Failed to delete")
             
-            message = await websocket.receive_text()
-            if message == 'R':
-                try:
-                    logger.info(f"Received message Socket {message}", exc_info=True)
-                    serial_port.write(bytes(message, 'utf-8'))
-                    serial_port.flush()
-                    data = serial_port.readline()
-                    data = data.decode()
-                    if data != '':
-                        logger.info("Received Data from Serial Port: {}".format(data))
-                    serial_port.flushInput()                    
-                except Exception as e:
-                    logger.error(f"Error in sending data {e}", exc_info=True)
-            else:
-                logger.info(f"Received message from Socket {message}", exc_info=True)
-                
-                if message == 'train':
-                    logger.info(f"Start Training new dataset")
-                    subprocess.Popen(["python", "training.py"])
-            
-            if message.isnumeric():
-                try:
-                    serial_port.write(bytes(message, 'utf-8'))
-                    serial_port.flush()
-                    logger.info(f"Sendig message {message}")
-                except Exception as e:
-                    logger.error(f"Error in sending data {e}", exc_info=True)
-            
-
     except Exception as e:
         logger.error(f"Error message f{e}", exc_info=True)
         # await websocket.close()
+def get_fingerprint():
+    """Get a finger print image, template it, and see if it matches!"""
+    print("Waiting for image...")
+    while finger.get_image() != adafruit_fingerprint.OK:
+        pass
+    print("Templating...")
+    if finger.image_2_tz(1) != adafruit_fingerprint.OK:
+        return False
+    print("Searching...")
+    if finger.finger_search() != adafruit_fingerprint.OK:
+        return False
+    return True
+# pylint: disable=too-many-branches
+def get_fingerprint_detail():
+    """Get a finger print image, template it, and see if it matches!
+    This time, print out each error instead of just returning on failure"""
+    print("Getting image...", end="", flush=True)
+    i = finger.get_image()
+    if i == adafruit_fingerprint.OK:
+        print("Image taken")
+    else:
+        if i == adafruit_fingerprint.NOFINGER:
+            print("No finger detected")
+        elif i == adafruit_fingerprint.IMAGEFAIL:
+            print("Imaging error")
+        else:
+            print("Other error")
+        return False
+
+    print("Templating...", end="", flush=True)
+    i = finger.image_2_tz(1)
+    if i == adafruit_fingerprint.OK:
+        print("Templated")
+    else:
+        if i == adafruit_fingerprint.IMAGEMESS:
+            print("Image too messy")
+        elif i == adafruit_fingerprint.FEATUREFAIL:
+            print("Could not identify features")
+        elif i == adafruit_fingerprint.INVALIDIMAGE:
+            print("Image invalid")
+        else:
+            print("Other error")
+        return False
+
+    print("Searching...", end="", flush=True)
+    i = finger.finger_fast_search()
+    # pylint: disable=no-else-return
+    # This block needs to be refactored when it can be tested.
+    if i == adafruit_fingerprint.OK:
+        print("Found fingerprint!")
+        return True
+    else:
+        if i == adafruit_fingerprint.NOTFOUND:
+            print("No match found")
+        else:
+            print("Other error")
+        return False
+    
+
+# pylint: disable=too-many-statements
+async def enroll_finger(location,websocket: WebSocket):
+    """Take a 2 finger images and template it, then store in 'location'"""
+    for fingerimg in range(1, 3):
+        if fingerimg == 1:
+            print("Place finger on sensor...", end="", flush=True)
+            websocket.send_json({"command": "Place finger on sensor"})
+        else:
+            print("Place same finger again...", end="", flush=True)
+            await websocket.send_json({"command": "Place same finger again"})
+
+        while True:
+            i = finger.get_image()
+            if i == adafruit_fingerprint.OK:
+                print("Image taken")
+                
+                break
+            if i == adafruit_fingerprint.NOFINGER:
+                await websocket.send_json({"command": "Place Your Finger"})
+                print(".", end="", flush=True)
+            elif i == adafruit_fingerprint.IMAGEFAIL:
+                print("Imaging error")
+                return False
+            else:
+                print("Other error")
+                return False
+
+        print("Templating...", end="", flush=True)
+        i = finger.image_2_tz(fingerimg)
+        if i == adafruit_fingerprint.OK:
+            print("Templated")
+        else:
+            if i == adafruit_fingerprint.IMAGEMESS:
+                print("Image too messy")
+            elif i == adafruit_fingerprint.FEATUREFAIL:
+                print("Could not identify features")
+            elif i == adafruit_fingerprint.INVALIDIMAGE:
+                print("Image invalid")
+            else:
+                print("Other error")
+            return False
+
+        if fingerimg == 1:
+            print("Remove finger")
+            await websocket.send_json({"command": "Place Your Finger"})
+            time.sleep(1)
+            while i != adafruit_fingerprint.NOFINGER:
+                i = finger.get_image()
+
+    print("Creating model...", end="", flush=True)
+    await websocket.send_json({"command": "Creating model..."})
+    i = finger.create_model()
+    if i == adafruit_fingerprint.OK:
+        print("Created")
+    else:
+        if i == adafruit_fingerprint.ENROLLMISMATCH:
+            print("Prints did not match")
+            await websocket.send_json({"command": "Prints did not match"})
+        else:
+            print("Other error")
+        return False
+
+    print("Storing model #%d..." % location, end="", flush=True)
+    i = finger.store_model(location)
+    if i == adafruit_fingerprint.OK:
+        print("Stored")
+        await websocket.send_json({"command": "Success"})
+        await websocket.send_json({"id": location})
+    else:
+        if i == adafruit_fingerprint.BADLOCATION:
+            print("Bad storage location")
+        elif i == adafruit_fingerprint.FLASHERR:
+            print("Flash storage error")
+        else:
+            print("Other error")
+        return False
+
+    return True
+##################################################
+
+
+
+def get_num():
+    """Use input() to get a valid number from 1 to 127. Retry till success!"""
+    i = 0
+    while (i > 127) or (i < 1):
+        try:
+            #i = int(input("Enter ID # from 1-127: "))
+            i=random.randint(0, 128) 
+        except ValueError:
+            pass
+    return i
+
+
 
 
 if __name__ == '__main__':
--- appfolder/app.py
+++ appfolder/app.py
@@ -14,14 +14,8 @@ import asyncio
 from threading import Thread
 import subprocess
 
-import time
-import board
-#import busio
-from digitalio import DigitalInOut, Direction
-import adafruit_fingerprint
+import websockets
 
-led = DigitalInOut(board.D13)
-led.direction = Direction.OUTPUT
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
@@ -44,9 +38,20 @@ clients = set()
 async  def index(request):
     return FileResponse(st_abs_file_path + "index.html")
 
-uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
+ports = get_serial_ports()
 
-finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
+_port = None
+serial_port = None
+for port, desc, hwid in sorted(ports):
+    logger.info("{}: {} [{}]".format(port, desc, hwid))
+    if "USB-Serial" in desc:
+        _port = port
+        break
+
+
+
+serial_port = serial.Serial(_port, baudrate=9600, timeout=0)
+
 
 @app.websocket_route("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -64,27 +69,49 @@ async def websocket_endpoint(websocket: WebSocket):
 
             async def read_serial():
                 while is_running:
-                    print("----------------")
-                    if finger.read_templates() != adafruit_fingerprint.OK:
-                        raise RuntimeError("Failed to read templates")
-                    print("Fingerprint templates:", finger.templates)
-                    print("e) enroll print")
-                    print("f) find print")
-                    print("d) delete print")
-                    print("----------------")
 
-                    
+                    try:
+                        if serial_port.inWaiting() > 0:
+                            data = serial_port.readline()
+                            data = data.decode()
+                            await websocket.send_text(data)
+                            logger.info("Received Data from Serial Port: {}".format(data))
+                            serial_port.flushInput()
+                    except Exception as e:
+                        try:
+                            if serial_port is not None and not serial_port.is_open:
+                                serial_port.open()
+                        except Exception as e:
+                            pass
+                        logger.error(f"Error in sending data {e}", exc_info=True)
             
             message = await websocket.receive_text()
             if message == 'R':
-                logger.info(f"Start Training new dataset")
-                subprocess.Popen(["python", "fingerprintAdafruit.py"])
+                try:
+                    logger.info(f"Received message Socket {message}", exc_info=True)
+                    serial_port.write(bytes(message, 'utf-8'))
+                    serial_port.flush()
+                    data = serial_port.readline()
+                    data = data.decode()
+                    if data != '':
+                        logger.info("Received Data from Serial Port: {}".format(data))
+                    serial_port.flushInput()                    
+                except Exception as e:
+                    logger.error(f"Error in sending data {e}", exc_info=True)
             else:
                 logger.info(f"Received message from Socket {message}", exc_info=True)
                 
                 if message == 'train':
                     logger.info(f"Start Training new dataset")
                     subprocess.Popen(["python", "training.py"])
+            
+            if message.isnumeric():
+                try:
+                    serial_port.write(bytes(message, 'utf-8'))
+                    serial_port.flush()
+                    logger.info(f"Sendig message {message}")
+                except Exception as e:
+                    logger.error(f"Error in sending data {e}", exc_info=True)
             
 
     except Exception as e:
