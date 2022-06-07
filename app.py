25dce81a7fbec9099fc3048a743b3956e22f5981
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-07 09:33:40 +0300

    chnges made

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ import random
 #import busio
 from digitalio import DigitalInOut, Direction
 import adafruit_fingerprint
+from cv2 import cv2
+import os
+import time
+
 
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
@@ -84,10 +88,60 @@ async def websocket_endpoint(websocket: WebSocket):
                     print("Deleted!")
                 else:
                     print("Failed to delete")
-            
+            if c == "t":
+                await createDataset(websocket)
+           
     except Exception as e:
         logger.error(f"Error message f{e}", exc_info=True)
         # await websocket.close()
+async def createDataset(websocket):
+    cam_port = 0
+
+    #get current directory
+    current_directory=os.getcwd()
+
+    #join to dataset director
+    new_directory=os.path.join(current_directory, "datasets")
+
+    #change dir to dataset
+    os.chdir(new_directory)
+
+    #input dir name
+    name = input('Enter voter name: ')
+
+    #create voters dir
+    os.mkdir(name)
+
+    #get voter dir
+    voter_dir=os.getcwd()
+
+    #join to dataset director
+    voter_dir=os.path.join(new_directory, name)
+
+    #change dir to voter dir
+    os.chdir(voter_dir)
+
+    #change to voter dir
+    os.chdir(voter_dir)
+    time.sleep(10)
+    for x in range(10):
+        #capture image
+        cam = cv2.VideoCapture(cam_port)
+
+        #save image
+        
+        result, image = cam.read()
+        if result:
+            cv2.imshow(name+""+str(x), image)
+            cv2.imwrite(name+""+str(x)+".png", image)
+            
+            cv2.destroyWindow(name)
+            time.sleep(10)
+        else:
+            print("No image detected. Please! try again")
+        os.listdir()
+    
+    
 async def get_fingerprint(websocket):
     await websocket.send_json({"command": "Place finger on sensor"})
     while finger.get_image() != adafruit_fingerprint.OK:
