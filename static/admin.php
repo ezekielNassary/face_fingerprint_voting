5254aa21fe5662dce8fddaa6df6fd4cc7c91614b
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-12 17:14:44 +0300

    vote

--- static/admin.php
+++ static/admin.php
@@ -21,8 +21,8 @@
 <body class="bg-light text-dark">
   <div class="container ">
     <div class="row ">
-      <div class="col-md-3"></div>
-      <div class="col-md-6 shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="col-md-2"></div>
+      <div class="col-md-8 shadow-sm p-3 mb-5 bg-white rounded">
         <h3 class="text-center p-3">ELECTRONIC VOTING</h3>
         <div>
           <p class=" bg-primary p-2 text-center text-light">Admin Control Panel</p>
@@ -64,8 +64,7 @@
                 <th>Gender</th>
                 <th>Phone</th>
                 <th>Vote ID</th>
-             
-                <th>Action</th> 
+                             <th>Action</th> 
              </tr>
     </thead>
     <tbody>';
@@ -103,7 +102,7 @@
 
 
             </div>
-            <div class="col-md-3"></div>
+            <div class="col-md-2"></div>
           </div>
         </div>
 
