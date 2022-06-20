dcc874186469815f84ed2a844be2d634f2e4fa03
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-20 18:11:38 +0300

    added some files

--- app.py
+++ app.py
@@ -139,13 +139,14 @@ async def websocket_endpoint(websocket: WebSocket):
                         # 2022-06-20T15:56:43 EAT [6109] INFO :
                         # 2022-06-20T15:56:45 EAT [6109] INFO : happy
                         command=realtime_output[38:45]
-                        time.sleep(1)
+                        
                         print(x)
                         if command == '':
                             command='No person'
                         else:
                             await websocket.send_json({"face_status": command})
                             print(command)
+                            time.sleep(1)
                             if x==19:
                                 cv2.destroyAllWindows()
                                 return True
