b57a7cdb92631a86478c9f8ba0aa94f96ea3970b
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-20 18:34:25 +0300

    added some files

--- app.py
+++ app.py
@@ -149,6 +149,7 @@ async def websocket_endpoint(websocket: WebSocket):
                             time.sleep(1)
                             if x==19:
                                 cv2.destroyAllWindows()
+                                cv2.waitKey(1)
                 return True
                             
                             
