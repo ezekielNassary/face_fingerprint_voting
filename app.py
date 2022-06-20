f4df12182bf70bafa139fc180aaaaf9336b0be72
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-20 17:15:05 +0300

    added some files

--- app.py
+++ app.py
@@ -121,6 +121,7 @@ async def websocket_endpoint(websocket: WebSocket):
                             # await websocket.send_json({"command": realtime_output})
                 except subprocess.CalledProcessError:
                     print("Timeout expired!")
+                break
                    
                     
                 
