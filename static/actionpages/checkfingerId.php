f7ac2548dcb0dd2249d838397d7451574fc76596
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-25 16:38:01 +0300

    admin and result pages apdating

--- static/actionpages/checkfingerId.php
+++ static/actionpages/checkfingerId.php
@@ -1,16 +1,21 @@
 <?php
- header("Access-Control-Allow-Origin: *");
- header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 require_once "config.php";
-$fingerId = mysqli_real_escape_string($link, $_POST['fingerId']);
-
-$sql = "SELECT * FROM voters WHERE Fingerid='$fingerId' ";
-$result = mysqli_query($link, $sql);
-if (mysqli_num_rows($result) >0) {
-while ($row = mysqli_fetch_array($result)) {
-    $name = $row['Name'];
-    $fingerid = $row['Fingerid'];
-  echo $name;
-}}else {
-    echo "No Results Found.";
+$fingerId = "";
+if (isset($_POST['fingerId'])) {
+    $fingerId = mysqli_real_escape_string($link, $_POST['fingerId']);
+    $sql = "SELECT * FROM voters WHERE Fingerid='$fingerId' ";
+    $result = mysqli_query($link, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $name = $row['Name'];
+            $fingerid = $row['Fingerid'];
+            echo $name;
+        }
+    } else {
+        echo "No Results Found.";
     }
+} else {
+    echo "id not sent";
+}
