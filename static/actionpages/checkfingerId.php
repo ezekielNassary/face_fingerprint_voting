2a1e1dec43a69665faa838bc2cb47254ad4b74a0
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-06-25 17:32:28 +0300

    admin and result pages apdating

--- static/actionpages/checkfingerId.php
+++ static/actionpages/checkfingerId.php
@@ -7,13 +7,11 @@ if (isset($_POST['fingerId'])) {
     $fingerId = mysqli_real_escape_string($link, $_POST['fingerId']);
     $sql = "SELECT * FROM voters WHERE Fingerid='$fingerId' ";
     $result = mysqli_query($link, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
+   if($row = mysqli_fetch_array($result)) {
             $name = $row['Name'];
             $fingerid = $row['Fingerid'];
             echo $name;
-        }
-    } else {
+        } else {
         echo "Not found";
     }
 } else {
